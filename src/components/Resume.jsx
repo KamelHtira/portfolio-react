0103fad776c65ba8f7c0d4a4b9e349f5e1ff2231
KamelHtira <Kamel.htira@isamm.u-manouba.tn>
2024-04-05 18:14:49 +0100

    fix compile errors

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import resumeFile from "../documents/KAMEL_HTIRA_CV.pdf";
+// import resumeFile from "../documents/KAMEL_HTIRA_CV.pdf";
 
 const Resume = ({ classicHeader, darkTheme }) => {
   const educationDetails = [
